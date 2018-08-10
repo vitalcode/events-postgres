beed821d658048a2471ae07a4f8c3b5827ac621e
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2018-08-10 23:10:53 +0100

    Update default user schema

--- init.sh
+++ init.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" <<-EOSQL
-    CREATE TABLE users(
+    CREATE TABLE user (
         id          BIGSERIAL PRIMARY KEY,
-        username    VARCHAR NOT NULL,
+        uid         VARCHAR NOT NULL UNIQUE,
+        name        VARCHAR NOT NULL UNIQUE,
+        email       VARCHAR NOT NULL UNIQUE,
         password    VARCHAR NOT NULL,
+        status      VARCHAR NOT NULL,
+        vat         VARCHAR,
         permissions VARCHAR
     );
-    INSERT INTO users(username, password) values('$EVENTS_APP_USER', '$EVENTS_APP_PASSWORD');
+    INSERT INTO users(uid, name, email, password, status, permissions)
+        values('dbe90976-6ead-4678-870a-c4579b0cb91b', '$EVENTS_APP_USER', '$EVENTS_APP_USER', '$EVENTS_APP_PASSWORD', 'Active', 'admin');
 EOSQL
