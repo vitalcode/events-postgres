8bb9fb77f2cf0456ce1011899f68dbcfd6fa32fd
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2018-08-12 23:07:10 +0100

    Update default user schema including access and refresh token properties

--- init.sh
+++ init.sh
@@ -2,14 +2,19 @@
 
 psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" <<-EOSQL
     CREATE TABLE "user" (
-      "id"          BIGSERIAL PRIMARY KEY,
-      "uid"         VARCHAR NOT NULL UNIQUE,
-      "name"        VARCHAR NOT NULL UNIQUE,
-      "email"       VARCHAR NOT NULL UNIQUE,
-      "password"    VARCHAR NOT NULL,
-      "status"      VARCHAR NOT NULL,
-      "vat"         VARCHAR,
-      "permissions" VARCHAR
+      "id"                  BIGSERIAL PRIMARY KEY,
+      "uid"                 VARCHAR NOT NULL UNIQUE,
+      "name"                VARCHAR NOT NULL UNIQUE,
+      "email"               VARCHAR NOT NULL UNIQUE,
+      "password"            VARCHAR NOT NULL,
+      "status"              VARCHAR NOT NULL,
+      "vat"                 VARCHAR,
+      "permissions"         VARCHAR,
+      "hmrc_access_token"   VARCHAR,
+      "hmrc_token_type"     VARCHAR,
+      "hmrc_expires_in"     VARCHAR,
+      "hmrc_refresh_token"  VARCHAR,
+      "hmrc_scope"          VARCHAR
     );
     INSERT INTO "user"("uid", "name", "email", "password", "status", "permissions")
         values('dbe90976-6ead-4678-870a-c4579b0cb91b', '$EVENTS_APP_USER', '$EVENTS_APP_USER', '$EVENTS_APP_PASSWORD', 'Active', 'admin');
