5032dbd5de83108a404f55e541195828e6c66f5a
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2018-09-14 13:40:20 +0100

    Add agent column to user table

--- init.sh
+++ init.sh
@@ -4,10 +4,11 @@ psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" <<-EOSQL
     CREATE TABLE "user" (
       "id"                  BIGSERIAL PRIMARY KEY,
       "uid"                 VARCHAR NOT NULL UNIQUE,
-      "name"                VARCHAR NOT NULL UNIQUE,
       "email"               VARCHAR NOT NULL UNIQUE,
+      "name"                VARCHAR NOT NULL,
       "password"            VARCHAR NOT NULL,
       "status"              VARCHAR NOT NULL,
+      "agent"               BOOLEAN NOT NULL,
       "vat"                 VARCHAR,
       "permissions"         VARCHAR,
       "hmrc_access_token"   VARCHAR,
@@ -16,6 +17,7 @@ psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" <<-EOSQL
       "hmrc_refresh_token"  VARCHAR,
       "hmrc_scope"          VARCHAR
     );
-    INSERT INTO "user"("uid", "name", "email", "password", "status", "permissions")
-        values('dbe90976-6ead-4678-870a-c4579b0cb91b', '$EVENTS_APP_USER', '$EVENTS_APP_USER', '$EVENTS_APP_PASSWORD', 'Active', 'admin');
+    INSERT INTO "user"("uid", "email", "name", "password", "status", "agent", "permissions")
+        values('dbe90976-6ead-4678-870a-c4579b0cb91b', '$EVENTS_APP_USER', '$EVENTS_APP_USER', '$EVENTS_APP_PASSWORD', 'Active', FALSE, 'admin');
 EOSQL
+
